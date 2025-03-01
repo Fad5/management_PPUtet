7cb7ef5ea5d1baba3f7b8de6019edebd8dcebcb1
andre <andreyka.anokhin@mail.ru>
2025-03-01 20:29:01 +0300

    Добавления документации к функциям

--- Create_dir.py
+++ Create_dir.py
@@ -2,17 +2,39 @@ import check_files
 import os
 from config import SAVE_DIR
 
-list_files = check_files.logic_check()
 
-name_sample = []
-for i in list_files:
-    name_sample.append(i['Name'])
+def get_data_in_js() -> list:
+    """
+    Функция запускает логику работы проверки фалов
+    которые есть на экофизике и в таблице google sheets,
+    получает json, заберет только название для дальнейшего создания папок
+
+    Возвращает:
+    - list_files - список названия папок которые нужно создать
+    """
+    list_files = check_files.logic_check()
+    name_sample = []
+    for i in list_files:
+        name_sample.append(i['Name'])
+    return list_files
 
 
-def create_dir(list_name):
-    for i in list_name:
-        os.makedirs(SAVE_DIR + '/' + str(i), exist_ok=True)
+def create_dir(list_names: list) -> None:
+    """
+    Функция создания папок, на вход принимает список с названиями фалов
+    """
+    for name in list_names:
+        os.makedirs(SAVE_DIR + '/' + str(name), exist_ok=True)
 
 
-def logic_dir():
-    create_dir(list(set(name_sample)))
+def logic_dir() -> None:
+    """
+    Логика работы функции
+    """
+    list_name_dir = get_data_in_js()
+    # Получает список, переводит в set, для того чтобы получить уникальные значения
+    # И обратно переводит в list для дальнейше работы
+    create_dir(list(set(list_name_dir)))
+
+
+logic_dir()
